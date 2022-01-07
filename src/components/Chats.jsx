e6d798c8ab718af827ed20dc11fbe287501ac1ad
shishi-mountain <chiichiitei_0711@yahoo.co.jp>
2022-01-07 18:08:55 +0900

    #8

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,12 +3,12 @@ import List from '@material-ui/core/List';
 import {Chat} from './index'
 import {createStyles, makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) =>
+const useStyles = makeStyles(() =>
     createStyles({
-        root: {
-            width: "100%",
-            maxWidth: "36ch",
-            backgroundColor: theme.palette.background.paper,
+        "chats": {
+            height: "400px",
+            padding: "0",
+            overflow: "auto"
         }
     }),
 );
@@ -17,7 +17,7 @@ const Chats = (props) => {
     const classes = useStyles();
 
     return (
-        <List className={classes.root}>
+        <List className={classes.chats} id={"scroll-area"}>
             {props.chats.map((chat, index) => {
                 return <Chat text={chat.text} type={chat.type} key={index.toString()}/>
             })}
